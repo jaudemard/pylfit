9417b95a7bf2d97981d0ef391a9508d4dff95a41
jaudemard <juliette.audemard@ec-nantes.fr>
2026-06-29 14:41:41 +0200

    train on one target only

--- tests/examples/api_pkn_example.py
+++ tests/examples/api_pkn_example.py
@@ -56,7 +56,8 @@ if __name__ == "__main__":
     with open(dataset_path,"r") as data_file:
         header = data_file.readline().strip().split(',')
     features = [feature for feature in header if "t_1" in feature]
-    targets = [target for target in header if "t_1" not in target]
+    # targets = [target for target in header if "t_1" not in target]
+    targets = ['CycA_t']
 
     full_dataset = discrete_state_transitions_dataset_from_csv(dataset_path, feature_names=features, target_names=targets)
     all_observed_states = [transition[0] for transition in full_dataset.data]
@@ -67,17 +68,17 @@ if __name__ == "__main__":
     ind = random.sample(range(0,len(full_dataset.data)),int(0.8*len(full_dataset.data)))
     _data = [full_dataset.data[i] for i in ind]
     
-    train_ind = random.sample(ind,int(0.8*len(_data)))
+    train_ind = random.sample(ind,len(_data))
 
     train_set = DiscreteStateTransitionsDataset([full_dataset.data[i] for i in train_ind], full_dataset.features, full_dataset.targets)
-    validation_set = DiscreteStateTransitionsDataset([full_dataset.data[i] for i in ind if i not in train_ind], full_dataset.features, full_dataset.targets)
+    # validation_set = DiscreteStateTransitionsDataset([full_dataset.data[i] for i in ind if i not in train_ind], full_dataset.features, full_dataset.targets)
 
     _data = [full_dataset.data[i] for i in range(0, len(full_dataset.data)) if i not in ind]
     test_set = [transition[0] for transition in _data]
 
     logging.info("\nGenerating partial datasets...")
 
-    dataset_sample_size = [0.02,0.05,0.06,0.1]
+    dataset_sample_size = [0.01,0.02,0.05,0.1,0.20]
     partial_datasets = []
 
     for sample_size in dataset_sample_size:
@@ -106,12 +107,11 @@ if __name__ == "__main__":
 
     # ----------------------------------------
     # Test 1
-    # Expanging generality of the final model by covering unobserved states with prior knowledge
+    # Expanding generality of the final model by covering unobserved states with prior knowledge
     # ----------------------------------------
     logging.info(f"{separator}Test 1")
-    logging.info(f"Expanding generalisability of the program by covering unobserved states with prior knowledge{separator}")   
-    
-     
+    logging.info(f"Expanding generalisability of the program by covering unobserved states with prior knowledge{separator}")
+
 
     # rules_percentages = [0.1,0.2,0.3,0.5,1]
 
@@ -120,7 +120,44 @@ if __name__ == "__main__":
     #     index = random.sample(range(0,len(reference_rules)),int(percentage*len(reference_rules)))
     #     partial_prior_knowledge.append([reference_rules[i] for i in index])
 
-    logging.info("\tlaunching learning on partial datasets, as control values...")
+    # ----------------------------------------
+    # Control Run
+
+    logging.info("\tlaunching control...")
+
+    test_1_control_accuracy = []
+    for i, partial_dataset in enumerate(partial_datasets):
+
+        logging.info(f"\tcontrol {i+1}, {dataset_sample_size[i]*100}% ({len(partial_dataset.data)})")
+        model = WDMVLP(features=partial_dataset.features, targets=partial_dataset.targets)
+        model.compile(algorithm="gula")
+        model.fit(partial_dataset)
+        pred = model.predict(test_set)
+
+        predicted_targets = {}
+        for feature_state in pred.keys():
+            predictions = pred[feature_state]
+            max_score_targets = []
+            for target in targets:
+                values = list(predictions[target].keys())
+                score = [predictions[target][value][0] for value in values]
+                max_score_targets.append(values[score.index(max(score))])
+            predicted_targets[feature_state] = max_score_targets
+
+        # print("TRUE: ", targets_true)
+        # print("PRED: ", predicted_targets)
+
+        accuracy = compute_accuracy(targets_true, predicted_targets)
+
+        logging.info(f"\tAccuracy: {accuracy}")
+
+
+    # ----------------------------------------
+    # PKN Run
+
+    logging.info("\tlaunching PKN integration...")
+
+
 
     test_1_control_accuracy = []
     for i, partial_dataset in enumerate(partial_datasets):
