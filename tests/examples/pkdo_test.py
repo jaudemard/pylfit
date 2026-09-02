3804f2d0e23acbc9b06f798752297cab62786593
jaudemard <juliette.audemard@ec-nantes.fr>
2026-09-02 10:40:24 +0200

    Modify hyperparameters

--- tests/examples/pkdo_test.py
+++ tests/examples/pkdo_test.py
@@ -12,13 +12,13 @@ import sys
 # Hyper Parameters 
 # ---------------------------------------------------------
 
-NOISE = 0
+NOISE = 0.1
 
-ACCURACY_THRESHOLD = 0.90
+ACCURACY_THRESHOLD = 0.95
 
 OBSERVATION_COVERAGE = 1.00
 
-BACKGROUND_RULES = []
+BACKGROUND_RULES = ["Rb_t(1) :- ."]
 
 # Dataset Load 
 # ---------------------------------------------------------
@@ -29,7 +29,7 @@ with open("tests/datasets/mammalian.csv","r") as data_file:
 features = [f for f in header if "t_1" in f]
 targets = [t for t in header if t not in features]
 
-dataset = discrete_state_transitions_dataset_from_csv(path="tests/datasets/mammalina.csv", feature_names=features, target_names=targets)
+dataset = discrete_state_transitions_dataset_from_csv(path="tests/datasets/mammalian.csv", feature_names=features, target_names=targets)
 
 # Add noise
 noisy_dataset = add_noise(dataset, NOISE)
@@ -42,15 +42,19 @@ background_rules = [Rule.from_string(rule,noisy_dataset.features, noisy_dataset.
 # PKDO Learning phase
 # ---------------------------------------------------------
 
-learned_rules = PKDO.fit(noisy_dataset, background_rules, ACCURACY_THRESHOLD, True)
+learned_rules = PKDO.fit(noisy_dataset, background_rules, ACCURACY_THRESHOLD, verbose = True)
 
 # Summary
 # ---------------------------------------------------------
-print("\n\n---------Summary----------\n")
-print(f"{len(learned_rules)} rules found.")
+print("\n\n---------Summary----------")
+if len(learned_rules) <=1:
+    r = "rule"
+else:
+    r = "rules"
+print(f"{len(learned_rules)} {r} found:")
 
 for rule in learned_rules:
-    print(rule)
+    print("\t", rule)
 
 real_rules = []
 with open("tests/tmp/mammalian_rules.txt","r") as real_rule_file:
@@ -65,8 +69,12 @@ with open("tests/tmp/mammalian_rules.txt","r") as real_rule_file:
 
 
 real_rules_accuracy = []
-for rule in real_rules_accuracy:
+for rule in real_rules:
     positives, negatives = PKDO.interprete(noisy_dataset, rule.head)
     real_rules_accuracy.append(PKDO._accuracy(rule, positives, negatives))
 
 summary = pd.DataFrame({"rule": real_rules, "score": real_rules_accuracy})
+
+with open("tests/tmp/learned_rules.txt", "w+") as learned_file:
+    for rule in learned_rules:
+        learned_file.write(f"{rule}\n")
